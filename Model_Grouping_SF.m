8d0725783e1973003d1a48d170d04f47d4fe6566
Alice Mason <am8302@bristol.ac.uk>
2015-05-05 15:46:40 +0800

    probability of item recall

--- Model_Grouping_SF.m
+++ Model_Grouping_SF.m
@@ -5,27 +5,31 @@ phi_p = 0.1;
 phi_g=0.3;
 gamma=0.97% rate of change of primacy gradient across groups
 sigma_gp=0.02%
-sigma_v=0.02;
+sigma_v=0.005;
 rho=0.3;
+theta=0.003;
 
 % experimental details
-nTrials=10;
+nTrials=1000;
 listlength=12;
 possGroupSize=1:5;
 
 v=zeros(nTrials, listlength); % this can be easily pre-allocated, so should be
-r=zeros(nTrials, listlength); % this can be easily pre-allocated, so should be
+    recalled_item=zeros(nTrials,1);
 
+N_O=0; % counter for non recalled items
 for t=1:nTrials
+    
+    r=zeros(1,listlength); 
     % Set the group sizes
     % moved following here
     %numGroups = 4; % number of groups - did we decide that there needed to be 4
-% or should this be random too?
-%    groupSize=randsample(possGroupSize,numGroups,true);
-%     while (sum(groupSize)>listlength || sum(groupSize)<listlength)
-%         groupSize=randsample(possGroupSize,numGroups,true);
-%     end
-
+    % or should this be random too?
+    %    groupSize=randsample(possGroupSize,numGroups,true);
+    %     while (sum(groupSize)>listlength || sum(groupSize)<listlength)
+    %         groupSize=randsample(possGroupSize,numGroups,true);
+    %     end
+    
     
     % Here is what happens in model
     % We generated a longish vector of group sizes, then find first
@@ -52,28 +56,47 @@ for t=1:nTrials
         gContext = [gContext repmat(gz,1,groupSize(gz))];
         pContext = [pContext linspace(0,1,groupSize(gz))];
     end
-    item_probe=listlength-(groupSize(numGroups))+1 % want to probe for first item of last group
-                                                % (what you had was close)
+    item_probe=listlength-(groupSize(numGroups))+1; % want to probe for first item of last group
+    % (what you had was close)
     
     v_GV = phi_g.^abs(gContext(item_probe)-gContext);
     v_PV = phi_p.^abs(pContext(item_probe)-pContext);
-
-    % sum group and item vectors 
+    
+    % sum group and item vectors
     t_v = rho*v_GV + (1-rho)*v_PV;
     v(t,:)=t_v;
-%Next step: implement item recall; 
-%for the moment, just assume recall of a single item using first item/last group as cue. 
-%Take average of the recalls, which will effectively be a first recall probability function.
-   
-for j=1:length(v)
-N=rand*sigma_v
-a(t,j)=(v(t,j)+N)*(1-r(t,j));
-end
+    %Next step: implement item recall;
+    %for the moment, just assume recall of a single item using first item/last group as cue.
+    %Take average of the recalls, which will effectively be a first recall probability function.
     
+
+    N=randn(1,listlength)*sigma_v;
+    a=(t_v+N).*(1-r);
+        %a(t,:)=(v(t,:)+N)*(1-r(t,:));
+
+    % activation of two highest items
+    % largest
+[max_value,max_idx] = max(a);  
+a(max_idx) = NaN; 
+second_max = max(a);  
+a = max_value;    
+
+
+diff=max_value-second_max;
+if diff>theta
+    recalled_item(t)=max_idx;
 end
+end
+
+% I tried to vectorise this but I'm not sure it's possible with "find"
+for i=1:listlength;
+    prop(i)=numel(find(recalled_item==i))/nTrials;
+end
+
+plot(1:i,prop)
+
 Av_v=mean(v);
 Av_a=mean(a);
-plot(Av_a);
 
 
 
