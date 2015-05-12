6d35e0662bc1f10cfc8600296f82d086fecdd9a3
Simon Farrell <simon.farrell@uwa.edu.au>
2015-05-12 10:22:13 +0800

    fixes to group cueing
    @am8302 have a look at changes on github so you can see what I’ve
    changed (or I think you can possibly do this in MATLAB)

--- Model_Grouping_SF.m
+++ Model_Grouping_SF.m
@@ -21,16 +21,15 @@ v=zeros(nTrials, listlength); % this can be easily pre-allocated, so should be
 recalled_item=zeros(nTrials,1);
 
 N_O=0; % counter for non recalled items
+
 for t=1:nTrials
     
     r=zeros(1,listlength);
-   
+    
     % Here is what happens in model
     % We generated a longish vector of group sizes, then find first
     % group that takes us beyond list length. We truncate that group
     % and use only the groups we need
-    
-    
     
     groupSize=randsample(possGroupSize,listlength,true);% random
     
@@ -43,34 +42,43 @@ for t=1:nTrials
     groupSize = groupSize(1:numGroups);
     
     lContext = ones(1,numGroups);     % control element for list context (each group has a list context)
-    List_cue = 1; % cues the list - at moment set to last group
+    List_cue = 1; % cues the list - at moment set to last LIST
     eta_LC=1+randn(1,numGroups)*sigma_L; % Eq A3
+    
     C_LC=eta_LC.*phi_l.^abs(List_cue-lContext); %Eq A7 - control element for list
     C_NC=zeros(1,numGroups); % control element for group cue
-    C_NC(List_cue)=eta_NC;
-    C=zeros(1,numGroups); 
+    C_NC(1)=eta_NC; % this shouldn't be list cue, but rather the
+                            % cue to a particular group (let's say the
+                            % first, but try setting it to last group)
+    %C=zeros(1,numGroups); % don't need this, as it is overwritten in next line
     C=C_NC+C_LC; % list and group control elements added
-    [max_value,max_idx]=max(C) % select most activated
-C(max_idx)=1; % set control element for most activared to 1
+    [max_value,max_idx]=max(C); % select most activated
+    C(max_idx)=1; % set control element for most activared to 1
+    % not sure what effect previous line has, as it isn't used below
     
     % make group markers
     gContext = [];
     pContext = [];
     absP = [];
     % set control elements
-
+    
     for gz=1:length(groupSize)
         gContext = [gContext repmat(gz,1,groupSize(gz))];
         pContext = [pContext linspace(0,1,groupSize(gz))];
         absP = [absP 1:groupSize(gz)];
     end
     
-    item_probe=listlength-(sum(groupSize(max_idx:end)))+1; % want to probe for first item of cued group
+    % commented out following line as it didn't make sense
+    %item_probe=listlength-(sum(groupSize(max_idx:end)))+1; % want to probe for first item of cued group
+    % instead, let's just use 1 for the moment, and soon we will
+    % implement sequential retrieval
+    item_probe = 1;
+    
     Group_cue=max_idx;% conext cued of the most activated group
     P_CG=1; % assume no effect of time
-
+    
     eta_gv=gamma.^(absP-1)+randn(1,listlength)*sigma_gp; %Eq A10
-    v_GV = P_CG*eta_gv.*phi_g.^abs(gContext(item_probe)-gContext); %Eq A11
+    v_GV = P_CG*eta_gv.*phi_g.^abs(Group_cue-gContext); %Eq A11
     v_PV = phi_p.^abs(pContext(item_probe)-pContext); %Eq A14
     
     % implemented primacy gradient
@@ -81,16 +89,16 @@ C(max_idx)=1; % set control element for most activared to 1
     v(t,:)=t_v;
     % noisy retrieval Eq A16
     noise=randn(1,listlength)*sigma_v;
-    a=(t_v+noise).*(1-r);    
+    a=(t_v+noise).*(1-r);
     % activation of two highest items
     [max_value,max_idx] = max(a);
     a(max_idx) = NaN;
     second_max = max(a);
     a = max_value; % retruns max_value into a
     
-        if (max_value-second_max)>theta
+    if (max_value-second_max)>theta
         recalled_item(t)=max_idx;
-        else
+    else
         recalled_item(t)=0;
     end
 end
